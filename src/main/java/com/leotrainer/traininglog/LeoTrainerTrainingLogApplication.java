cab1cbc8bb8ff87126c639a6baac8240a5fcf2e2
Juan D Cadavid <jcadavid@starbucks.com>
2024-01-26 11:52:29 -0500

    first commit

---
+++ src/main/java/com/leotrainer/traininglog/LeoTrainerTrainingLogApplication.java
@@ -0,0 +1,13 @@
+package com.leotrainer.traininglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeoTrainerTrainingLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeoTrainerTrainingLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leotrainer/traininglog/LeoTrainerTrainingLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leotrainer.traininglog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeoTrainerTrainingLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
